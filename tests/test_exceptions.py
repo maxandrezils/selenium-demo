b9018f707a36cb44eebd6316d3be8b8073b14f9d
Max-Andre Zils <max-andre.zils@takealot.com>
2022-12-12 12:48:16 +0200

    updated requirements

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -6,38 +6,42 @@ from selenium.webdriver.support import expected_conditions as ec
 
 
 class TestExceptions:
-    @pytest.mark.exceptions
-    def test_no_such_element_exception(self, driver):
-        # Open page
-        driver.get("https://practicetestautomation.com/practice-test-exceptions/")
 
-        # Click Add button
-        add_button_locator = driver.find_element(By.ID, "add_btn")
-        add_button_locator.click()
-        wait = WebDriverWait(driver, 10)
-        row_2_input_element = wait.until(
-            ec.presence_of_element_located((By.XPATH, "//div[@id='row2']/input"))
-        )
-        # Verify Row 2 input field is displayed
-        assert (
-            row_2_input_element.is_displayed()
-        ), "Row 2 input should be displayed, but it's not"
+	@pytest.mark.exceptions
+	def test_no_such_element_exception(self, driver):
+		# Open page
+		driver.get("https://practicetestautomation.com/practice-test-exceptions/")
 
-    @pytest.mark.exceptions
-    @pytest.mark.debug
-    def test_element_not_interacterable_exception(self, driver):
-        # Open page
-        driver.get("https://practicetestautomation.com/practice-test-exceptions/")
-        # Click Add button
-        add_button_locator = driver.find_element(By.ID, "add_btn")
-        add_button_locator.click()
-        wait = WebDriverWait(driver, 10)
-        row_2_input_element = wait.until(
-            ec.presence_of_element_located((By.XPATH, "//div[@id='row2']/input"))
-        )
-        row_2_input_element.send_keys("Sushi")
-        driver.find_element(By.NAME, "Save").click()
-        confirmation_element = driver.find_element(By.ID, "confirmation")
-        assert (
-            confirmation_element.text == "Row 2 was saved"
-        ), "Confirmation message is incorrect"
+		# Click Add button
+		add_button_locator = driver.find_element(By.ID, "add_btn")
+		add_button_locator.click()
+		wait = WebDriverWait(driver, 10)
+		row_2_input_element = wait.until(
+			ec.presence_of_element_located((By.XPATH, "//div[@id='row2']/input"))
+		)
+		# Verify Row 2 input field is displayed
+		assert (
+			row_2_input_element.is_displayed()
+		), "Row 2 input should be displayed, but it's not"
+
+	@pytest.mark.exceptions
+	@pytest.mark.debug
+	def test_element_not_interactable_exception(self, driver):
+		# Open page
+		driver.get("https://practicetestautomation.com/practice-test-exceptions/")
+		# Click Add button
+		add_button_locator = driver.find_element(By.ID, "add_btn")
+		add_button_locator.click()
+		wait = WebDriverWait(driver, 10)
+		row_2_input_element = wait.until(
+			ec.presence_of_element_located((By.XPATH, "//div[@id='row2']/input"))
+		)
+		row_2_input_element.send_keys("Sushi")
+		driver.find_element(
+			By.XPATH,
+			"//div[@id='rows']/div[3]/div[@class='row']/button[@id='save_btn']§",
+		).click()
+		confirmation_element = driver.find_element(By.ID, "confirmation")
+		assert (
+				confirmation_element.text == "Row 2 was saved"
+		), "Confirmation message is incorrect"
