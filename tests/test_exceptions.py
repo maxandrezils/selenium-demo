a45a774ce07c09b3abd1becea12630b87312edc4
Max-Andre Zils <max-andre.zils@takealot.com>
2022-12-12 10:18:34 +0200

    added expected condition to exception file

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,12 +1,22 @@
 import pytest
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 
 
 class TestExceptions:
+    @pytest.mark.exceptions
     def test_no_such_element_exception(self, driver):
+        # Open page
         driver.get("https://practicetestautomation.com/practice-test-exceptions/")
-        add_button_locator = driver.find_element_by_id(By.ID, "add_btn")
-        second_row_locator = driver.find_element(
-            By.CSS_SELECTOR, "div:nth-of-type(3) > .row > .input-field"
-        )
-        assert second_row_locator.is_displayed(), "Second row is not displayed."
+
+        # Click Add button
+        add_button_locator = driver.find_element(By.ID, "add_btn")
+        add_button_locator.click()
+        wait = WebDriverWait(driver, 10)
+        wait.until(ec.presence_of_element_located((By.XPATH, "//div[@id='row2']/input")))
+        # Verify Row 2 input field is displayed
+        row_2_input_locator = driver.find_element(By.XPATH, "//div[@id='row2']/input")
+        assert (
+            row_2_input_locator.is_displayed()
+        ), "Row 2 input should be displayed, but it's not"
